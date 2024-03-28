9458c147d96ee14918d197742b586bd39f106118
Sadreev Artem <sadreev@echo-company.ru>
2024-03-28 14:00:53 +0500

    planet-zdorovo complete

--- src/planet-health/planet-health.service.ts
+++ src/planet-health/planet-health.service.ts
@@ -8,38 +8,30 @@ export class PlanetHealthService {
     try {
       const browser = await puppeteer.connect({
         browserWSEndpoint:
-          'ws://127.0.0.1:9222/devtools/browser/89a41c35-9b00-4025-8645-e9f3473295cf',
+          'ws://127.0.0.1:9222/devtools/browser/d6768c9d-1b02-4642-a1d2-1ec4734aee98',
       });
       const page = await browser.newPage();
-      // await page.waitForNetworkIdle({ idleTime: 1000 });
-      // await page.goto('https://planetazdorovo.ru/tyumen/');
-      // await page.waitForSelector('.input-field');
-      // await page.type('.input-field > input', product);
-      // await page.waitForNetworkIdle({ idleTime: 1000 });
-      // const products = await page.evaluate(() => {
-      //   const elems = document.querySelectorAll('.card-list .item-card');
-      //   return Array.from(elems).map((resultItem) => {
-      //     const url = resultItem.querySelector('a')?.href;
-      //     const title = resultItem.querySelector('.this-full')?.textContent;
-      //     const price = resultItem.querySelector(
-      //       '.item-card-price-number',
-      //     )?.textContent;
-      //     return {
-      //       url,
-      //       title,
-      //       price,
-      //     };
-      //   });
-      // });
       page.setDefaultNavigationTimeout(2 * 60 * 1000);
+      await page.setRequestInterception(true);
+      page.on('request', (req) => {
+        if (
+          req.resourceType() == 'stylesheet' ||
+          req.resourceType() == 'font' ||
+          req.resourceType() == 'image' ||
+          req.resourceType() == 'script'
+        ) {
+          req.abort();
+        } else {
+          req.continue();
+        }
+      });
       await Promise.all([
         page.waitForNavigation(),
-        // page.goto(encodeURI('https://planetazdorovo.ru/tyumen')),
         page.goto(
           encodeURI(
             `https://planetazdorovo.ru/search/?sort=price&q=${product}`,
           ),
-          { timeout: 500 },
+          { waitUntil: 'domcontentloaded' },
         ),
       ]);
       const products = await page.$$eval(
